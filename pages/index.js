d2d4c180b13b4085ac143c1e1c75589a43f6ca2a
sitimalatania <sitimalatania@mail.ugm.ac.id>
2024-03-11 16:52:39 +0700

    feat : add projects

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import DefaultLayout from "@/components/layout/DefaultLayout";
 import Beranda from "@/components/feature/Beranda";
+import Project from "@/components/feature/Project";
 
 export default function Home() {
   return (
     <DefaultLayout title="Sitimalatania">
       <Beranda />
+      <Project />
     </DefaultLayout>
   );
 }
