3949be38c04d7eea9f3df740d4e405a02d5e52a0
Siti Malatania <74101266+sitimalatania@users.noreply.github.com>
2024-03-11 16:42:43 +0700

    Home (#5)
    * feat : add home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import DefaultLayout from "@/components/layout/DefaultLayout";
+import Beranda from "@/components/feature/Beranda";
 
 export default function Home() {
-  return <DefaultLayout title="Sitimalatania">Main</DefaultLayout>;
+  return (
+    <DefaultLayout title="Sitimalatania">
+      <Beranda />
+    </DefaultLayout>
+  );
 }
